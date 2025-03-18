a93fe335700d51afc96f3d76388751d669b5b7d3
Skardery <jacob.kielhans@gmail.com>
2025-03-18 15:13:01 +0100

    GLTF

--- prosjekt2/gltfmodel/src/pages/index.tsx
+++ prosjekt2/gltfmodel/src/pages/index.tsx
@@ -1,10 +1,13 @@
-export default function Home() {
-  
-    return (
-    <>
-      <main>
-        <div></div>
-      </main>
-    </>
+import ModelViewer from '@/components/ModelViewer';
+
+const Home: React.FC = () => {
+  const modelUrl = '/bmw/scene.gltf';
+
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <ModelViewer modelUrl={modelUrl} />
+    </div>
   );
-}
+};
+
+export default Home;
